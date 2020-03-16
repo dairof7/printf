8d54fc12c738a26f8094f56ae2ac53b2fbf80e4e
somarae8 <1493@holbertonschool.com>
2020-03-16 05:25:42 +0000

    add function

---
+++ number_functions.c
@@ -0,0 +1,49 @@
+/**
+ * f_int - function to handle integers
+ * @list: list type
+ * Return: int
+ */
+#include "holberton.h"
+#include <stdlib.h>
+int f_int(va_list list)
+{
+	int num_p;
+	int i;
+	int number;
+	int contador = 0;
+	int tam = 0;
+	int copia;
+	char *caracter;
+
+	number = va_arg(list, int);
+	copia = number;
+	if (number < 0)
+	{
+		_putchar('-');
+		tam = 1;
+		num_p = number * -1;
+		copia = copia * -1;
+	}
+	else
+	{
+		tam = 0;
+		num_p = number;
+	}
+	while (num_p / 10 > 0)
+	{
+		num_p = num_p / 10;
+		contador++;
+	}
+	caracter = malloc(sizeof(char) * contador);
+	for (i = contador; i >= 0; i--)
+	{
+		caracter[i] = copia % 10;
+		copia = copia / 10;
+	}
+	for (i = 0; i <= contador; i++)
+		_putchar(caracter[i] + '0');
+		contador = contador + tam;
+		contador--;
+	return (contador);
+}
+
